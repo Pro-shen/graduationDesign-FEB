5047b6048172c55ad05521544cfdc62f1d9f135d
沈城 matt <2513919186@qq.com>
2024-02-08 17:36:10 +0800

    修改了开机启动艺术字体

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,6 +16,12 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  基于深度卷积网络的餐饮管理系统启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+        System.out.println("(♥◠‿◠)ﾉﾞ  基于深度卷积网络的餐饮管理系统启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                "                                  __    __\n" +
+                "  ______ ____       _____ _____ _/  |__/  |_\n" +
+                " /  ___// ___\\     /     \\\\__  \\\\   __\\   __\\\n" +
+                " \\___ \\\\  \\___    |  Y Y  \\/ __ \\|  |  |  |\n" +
+                "/____  >\\___  >   |__|_|  (____  /__|  |__|\n" +
+                "     \\/     \\/          \\/     \\/    ");
     }
 }
