f3f741c92eb4f0ee8dbb48392ef88fae97c9f5ba
沈城 matt <2513919186@qq.com>
2024-02-08 17:40:12 +0800

    修改项目启动艺术字

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -17,11 +17,12 @@ public class RuoYiApplication
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  基于深度卷积网络的餐饮管理系统启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                "                                  __    __\n" +
-                "  ______ ____       _____ _____ _/  |__/  |_\n" +
-                " /  ___// ___\\     /     \\\\__  \\\\   __\\   __\\\n" +
-                " \\___ \\\\  \\___    |  Y Y  \\/ __ \\|  |  |  |\n" +
-                "/____  >\\___  >   |__|_|  (____  /__|  |__|\n" +
-                "     \\/     \\/          \\/     \\/    ");
+                "                                                        ,d      ,d\n" +
+                "                                                        88      88\n" +
+                ",adPPYba,  ,adPPYba,    88,dPYba,,adPYba,  ,adPPYYba, MM88MMM MM88MMM\n" +
+                "I8[    \"\" a8\"     \"\"    88P'   \"88\"    \"8a \"\"     `Y8   88      88\n" +
+                " `\"Y8ba,  8b            88      88      88 ,adPPPPP88   88      88\n" +
+                "aa    ]8I \"8a,   ,aa    88      88      88 88,    ,88   88,     88,\n" +
+                "`\"YbbdP\"'  `\"Ybbd8\"'    88      88      88 `\"8bbdP\"Y8   \"Y888   \"Y888\n");
     }
 }
